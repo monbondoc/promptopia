12462ff790dd7bb505930e5d3299389e5ee625a2
Raymond <monbondoc@gmail.com>
2023-11-20 11:16:38 +0800

    first commit

---
+++ next.config.js
@@ -0,0 +1,21 @@
+/* const { config } = require('next/dist/build/templates/pages')
+
+/** @type {import('next').NextConfig} */ 
+/* const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        domains: ['1h3.googleusercontent.com'],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config;
+    }
+}
+
+module.exports = nextConfig */
